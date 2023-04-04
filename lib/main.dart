d58fcde30d89bc933c0bde1dc6b65cbe54e773e0
HMZKA <hmzakh65@gmail.com>
2023-04-04 16:16:17 +0300

    login

--- lib/main.dart
+++ lib/main.dart
@@ -12,50 +12,98 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Column"),
-            centerTitle: true,
+        appBar: AppBar(
+          title: Text('My project'),
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                child: Column(
+                  children: [
+                    Text(
+                      'Login',
+                      style:
+                          TextStyle(fontSize: 35, fontWeight: FontWeight.w600),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Login to enjoy our app',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                          label: Text('Email'),
+                          prefixIcon: Icon(Icons.email),
+                          hintText: 'example@gmail.com',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15))),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      keyboardType: TextInputType.visiblePassword,
+                      decoration: InputDecoration(
+                          label: Text('Password'),
+                          prefixIcon: Icon(Icons.lock),
+                          suffixIcon: IconButton(
+                              onPressed: () {}, icon: Icon(Icons.visibility)),
+                          hintText: 'P@ssw0rd',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15))),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                child: Column(
+                  children: [
+                    Container(
+                      clipBehavior: Clip.antiAlias,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(25)),
+                      width: 250,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Login',
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Don't have account?",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        TextButton(onPressed: () {}, child: Text('Create one'))
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
-          // 1 + 1 +1 +1
-          body: SizedBox(
-            height: double.infinity,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  color: Colors.pink,
-                  child: Text(
-                    'Fourth Text',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-                Container(
-                  color: Colors.amber,
-                  child: Text(
-                    'Fourth Text',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-                Container(
-                  color: Colors.pink,
-                  child: Text(
-                    'Fourth Text',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          )),
+        ),
+      ),
     );
   }
 }
